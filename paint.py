1121ef167cf8665222a5e908593b05c3ad5123df
Mikhail Podolskiy <134136109+mikhailpodolskiy@users.noreply.github.com>
2024-07-27 12:44:50 +0300

    Update paint.py

--- paint.py
+++ paint.py
@@ -1,9 +1,13 @@
 from tkinter import *
+from tkinter.ttk import Scale
+from tkinter import  colorchooser,messagebox
+from tkinter.filedialog import asksaveasfilename
 import PIL.ImageGrab as ImageGrab
 color= 'white'
 i = 1
 
 class Paint:
+  pass
 
   
 
